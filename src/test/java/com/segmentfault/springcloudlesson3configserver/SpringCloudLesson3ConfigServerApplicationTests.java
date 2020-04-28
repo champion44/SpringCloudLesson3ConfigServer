b7e76e953acfccadf170cb89cf6ac2cec734d28d
champion44 <799159902@qq.com>
2020-04-28 14:47:18 +0800

    “first”

---
+++ src/test/java/com/segmentfault/springcloudlesson3configserver/SpringCloudLesson3ConfigServerApplicationTests.java
@@ -0,0 +1,10 @@
+package com.segmentfault.springcloudlesson3configserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudLesson3ConfigServerApplicationTests {
+
+
+
+}
